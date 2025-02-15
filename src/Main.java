a7d743dde4a7c8e5a210f5f8d2d7c9ed49995db9
JaggerV2026 <jagger04@icloud.com>
2025-02-15 17:10:30 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Commenting this out for testing purposes
+        //String MipsArguments = args[0];
+        //Test argument
+        String mipsArguments = "add $t2, $s6, $s4";
+        String regex = "[\s]";
+        String commasRemoved = mipsArguments.replace(",", "");
+        String[] argArray = commasRemoved.split(regex);
+        System.out.println(argArray.length);
+        for(String s : argArray){
+            System.out.println(s);
+        }
+    }
+}
