e9fb010a7b6728d1509b3e5bd3f0e73c4e63f0b9
JaggerV2026 <123402353+JaggerV2026@users.noreply.github.com>
2025-02-15 18:25:51 -0600

    Added J format reading and printing hex result

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,23 @@ public class Main {
         String regex = "[\s]";
         String commasRemoved = mipsArguments.replace(",", "");
         String[] argArray = commasRemoved.split(regex);
-        
+
+        int bitString = 0; //Bits to convert to hex
+        String hexArgument = ""; //Arguments represented with hex
+        int convertedHex = 0;
+        String hexReturn = ""; //Hex argument to print at end
+
+        //J Format. j
+        if(argArray.length == 2){
+            bitString = 2; //000010
+            hexArgument = argArray[1].substring(2);
+            convertedHex = Integer.parseInt(hexArgument, 16);
+            bitString = (bitString << 26) | convertedHex;
+
+        }
+
+        //Could be one line, but might be useful later
+        hexReturn = String.format("%08x", bitString);
+        System.out.println(hexReturn);
     }
 }
