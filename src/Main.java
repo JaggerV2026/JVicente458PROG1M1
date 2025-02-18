eb1f08bdd3fbd6f6d759d5a5875d75716b22ae5c
JaggerV2026 <123402353+JaggerV2026@users.noreply.github.com>
2025-02-18 12:11:04 -0600

    Implemented R format reading and fixed bugs with I format. Added JAR file

--- src/Main.java
+++ src/Main.java
@@ -41,13 +41,19 @@ public class Main {
         registerDict.put("$ra", 31);
 
         //Commenting this out for testing purposes
-        //String MipsArguments = args[0];
+        String mipsArguments = args[0];
         //Test argument
-        String mipsArguments = "ori $fp, $t6, 0xff23";
-        String regex = "[\s]";
-        String commasRemoved = mipsArguments.replace(",", "");
-        String[] argArray = commasRemoved.split(regex);
-
+        //String mipsArguments = "sw $t6, ($s2)";
+        String regex = "[\\s]";
+        String commasRemoved = mipsArguments.replaceAll(",", "");
+        String spacesRemoved = commasRemoved.replaceAll("\\s{2,}", " ");
+        String[] argArray = spacesRemoved.split(regex);
+        /*
+        Testing
+        for(String arg : argArray){
+            System.out.println(arg);
+        }
+        */
         String offsetCleaned;
         String[] offsetArray;
 
@@ -69,41 +75,81 @@ public class Main {
             bitString = 9; //001001
             bitString = (bitString << 5) | registerDict.get(argArray[2]);
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[3].contains("0x")){
+                hexArgument = argArray[3].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[3]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[3]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[3]);
+            }
         }
         else if(argArray[0].equals("andi")){
             bitString = 12; //001100
             bitString = (bitString << 5) | registerDict.get(argArray[2]);
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[3].contains("0x")){
+                hexArgument = argArray[3].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[3]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[3]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[3]);
+            }
         }
         else if(argArray[0].equals("beq")){
             bitString = 4; //000100
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
             bitString = (bitString << 5) | registerDict.get(argArray[2]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[3].contains("0x")){
+                hexArgument = argArray[3].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[3]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[3]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[3]);
+            }
         }
         else if(argArray[0].equals("bne")){
             bitString = 5; //000101
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
             bitString = (bitString << 5) | registerDict.get(argArray[2]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[3].contains("0x")){
+                hexArgument = argArray[3].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[3]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[3]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[3]);
+            }
         }
         else if(argArray[0].equals("lui")){
             bitString = 15; //001111
             bitString = (bitString << 5);
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[2].contains("0x")){
+                hexArgument = argArray[2].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[2]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[2]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[2]);
+            }
         }
         else if(argArray[0].equals("lw")){
             bitString = 35; //100011
@@ -115,17 +161,33 @@ public class Main {
             if(offsetArray[0].isEmpty()){
                 bitString = (bitString << 16);
             }
-            else {
+            else if(offsetArray[0].contains("0x")){
+                hexArgument = offsetArray[0].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(offsetArray[0]) < 0){
                 bitString = ((bitString + 1)<< 16) + (Integer.parseInt(offsetArray[0]));
+            }
+            else {
+                bitString = (bitString << 16) + (Integer.parseInt(offsetArray[0]));
             }
         }
         else if(argArray[0].equals("ori")){
             bitString = 13; //001101
             bitString = (bitString << 5) | registerDict.get(argArray[2]);
             bitString = (bitString << 5) | registerDict.get(argArray[1]);
-            hexArgument = argArray[3].substring(2);
-            convertedHex = Integer.parseInt(hexArgument, 16);
-            bitString = (bitString << 16) | convertedHex;
+            if(argArray[3].contains("0x")){
+                hexArgument = argArray[3].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(argArray[3]) < 0){
+                bitString = ((bitString + 1)<< 16) + (Integer.parseInt(argArray[3]));
+            }
+            else {
+                bitString = (bitString << 16) | Integer.parseInt(argArray[3]);
+            }
         }
         else if(argArray[0].equals("sw")){
             bitString = 43; //101011
@@ -137,8 +199,43 @@ public class Main {
             if(offsetArray[0].isEmpty()){
                 bitString = (bitString << 16);
             }
-            else {
+            else if(offsetArray[0].contains("0x")){
+                hexArgument = offsetArray[0].substring(2);
+                convertedHex = Integer.parseInt(hexArgument, 16);
+                bitString = (bitString << 16) | convertedHex;
+            }
+            else if(Integer.parseInt(offsetArray[0]) < 0){
                 bitString = ((bitString + 1)<< 16) + (Integer.parseInt(offsetArray[0]));
+            }
+            else {
+                bitString = (bitString << 16) + (Integer.parseInt(offsetArray[0]));
+            }
+        }
+        //R Format
+        //R Format, but works a bit differently
+        else if(argArray[0].equals("syscall")){
+            bitString = (bitString << 25) | 12;
+        }
+        else {
+            bitString = 0;
+            bitString = (bitString << 5) | registerDict.get(argArray[2]);
+            bitString = (bitString << 5) | registerDict.get(argArray[3]);
+            bitString = (bitString << 5) | registerDict.get(argArray[1]);
+            bitString = (bitString << 5);
+            if(argArray[0].equals("add")){
+                bitString = (bitString << 6) | 32; //100000
+            }
+            else if(argArray[0].equals("and")){
+                bitString = (bitString << 6) | 36; //100100
+            }
+            else if(argArray[0].equals("or")){
+                bitString = (bitString << 6) | 37; //100101
+            }
+            else if(argArray[0].equals("slt")){
+                bitString = (bitString << 6) | 42; //101010
+            }
+            else if(argArray[0].equals("sub")){
+                bitString = (bitString << 6) | 34; //100010
             }
         }
         //Could be one line, but might be useful later
