3481172e99ccb2e037fafe1c38a8ea70bfd09230
JaggerV2026 <123402353+JaggerV2026@users.noreply.github.com>
2025-02-17 21:05:38 -0600

    Added register dictionary

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Main {
     public static void main(String[] args) {
+       //Register Dictionary
+        Dictionary<String, Integer> registerDict = new Hashtable<>();
+        registerDict.put("$zero", 0);
+        registerDict.put("$at", 1);
+        registerDict.put("$v0", 2);
+        registerDict.put("$v1", 3);
+        registerDict.put("$a0", 4);
+        registerDict.put("$a1", 5);
+        registerDict.put("$a2", 6);
+        registerDict.put("$a3", 7);
+        registerDict.put("$t0", 8);
+        registerDict.put("$t1", 9);
+        registerDict.put("$t2", 10);
+        registerDict.put("$t3", 11);
+        registerDict.put("$t4", 12);
+        registerDict.put("$t5", 13);
+        registerDict.put("$t6", 14);
+        registerDict.put("$t7", 15);
+        registerDict.put("$s0", 16);
+        registerDict.put("$s1", 17);
+        registerDict.put("$s2", 18);
+        registerDict.put("$s3", 19);
+        registerDict.put("$s4", 20);
+        registerDict.put("$s5", 21);
+        registerDict.put("$s6", 22);
+        registerDict.put("$s7", 23);
+        registerDict.put("$t8", 24);
+        registerDict.put("$t9", 25);
+        registerDict.put("$k0", 26);
+        registerDict.put("$k1", 27);
+        registerDict.put("$gp", 28);
+        registerDict.put("$sp", 29);
+        registerDict.put("$fp", 30);
+        registerDict.put("$ra", 31);
+
         //Commenting this out for testing purposes
         //String MipsArguments = args[0];
         //Test argument
